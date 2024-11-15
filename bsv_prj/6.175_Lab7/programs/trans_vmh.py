090859ac0803fc261f67190c4f5058007d01917a
stefan_zjs <stefan_zjs@163.com>
2024-11-15 19:32:05 +0800

    change width

--- bsv_prj/6.175_Lab7/programs/trans_vmh.py
+++ bsv_prj/6.175_Lab7/programs/trans_vmh.py
@@ -3,19 +3,28 @@
 import sys
 
 if len(sys.argv) != 3:
-	print('Usage: ./trans_vmh [input vmh] [output vmh]')
-	raise
+    print('Usage: ./trans_vmh [input vmh] [output vmh]')
+    raise
 
 in_file = sys.argv[1]
 out_file = sys.argv[2]
 
 with open(in_file, 'r') as fin:
-	lines = fin.readlines();
+    lines = fin.readlines()
 
 with open(out_file, 'w') as fout:
     # fout.write(lines[0]);
-    for i in range(0, len(lines)):
-        val = lines[i][:-1].rstrip('\n');
-        fout.write(val[8:] + '\n');
-        fout.write(val[:8] + '\n');         
+    line_num = 0
+    inst_line = ''
+    for i in range(1, len(lines)):
+        val = lines[i][:-1].rstrip('\n')
+        inst_line = val + inst_line
+        # fout.write(val[8:]);
+        # fout.write(val[:8]);    
+        line_num += 1    
+        if(line_num == 8):
+            line_num = 0    
+            fout.write(inst_line);     
+            fout.write('\n');       
+            inst_line = ''    
 
